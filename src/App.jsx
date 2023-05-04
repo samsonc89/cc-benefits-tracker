5f4d75d473531ebff76360ac44f4671d58c5adaf
Samson Cheung <samsonc89@yahoo.com>
2023-05-04 14:26:17 -0700

    Rename variable for cardData

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import data from "./cardData.json";
+import cardData from "./cardData.json";
 
 const BenefitsTable = ({ cards }) => {
   let userCards = [];
@@ -53,7 +53,7 @@ const Benefit = ({ benefit }) => {
     </tr>
   );
 };
-const BENEFITS = data;
+const BENEFITS = cardData;
 
 function App() {
   return (
