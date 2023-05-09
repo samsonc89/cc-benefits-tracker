64edb3562d3b6c98591418b9f13363fa3548ec39
Samson Cheung <samsonc89@yahoo.com>
2023-05-08 20:21:37 -0700

    Change state name to better description

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -3,17 +3,17 @@ import "./benefit.css";
 
 /* eslint-disable react/prop-types */
 const Benefit = ({ benefit }) => {
-  const [status, setStatus] = useState(false);
+  const [completed, setCompleted] = useState(false);
 
   function finishBenefit() {
-    if (status === false) {
-      setStatus(true);
+    if (completed === false) {
+      setCompleted(true);
     } else {
-      setStatus(false);
+      setCompleted(false);
     }
   }
   return (
-    <tr className={status ? "done" : ""}>
+    <tr className={completed ? "done" : ""}>
       <td>{benefit.benefit}</td>
       <td>{benefit.expires}</td>
       <td>
