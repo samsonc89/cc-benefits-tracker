711727fb27e688f3f8c829086b9ea570bde13c77
Samson Cheung <samsonc89@yahoo.com>
2023-05-08 20:18:11 -0700

    Add function to change styling of text when user checks box

--- src/components/Benefit.jsx
+++ src/components/Benefit.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import "./benefit.css";
+
 /* eslint-disable react/prop-types */
 const Benefit = ({ benefit }) => {
+  const [status, setStatus] = useState("");
+
+  function finishBenefit() {
+    if (status === "") {
+      setStatus("done");
+    } else {
+      setStatus("");
+    }
+  }
   return (
-    <tr>
+    <tr className={status}>
       <td>{benefit.benefit}</td>
       <td>{benefit.expires}</td>
       <td>
-        <input type="checkbox" checked={benefit.used ? true : false} readOnly />
+        <input type="checkbox" onClick={() => finishBenefit()} />
       </td>
     </tr>
   );
