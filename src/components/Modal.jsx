8398cdfe38d6372c044ce3dd9d97821fea0d868a
Samson Cheung <samsonc89@yahoo.com>
2023-06-01 10:58:37 -0700

    Change styling of modal window

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,16 @@
 const Modal = ({ onClick, state, change }) => {
   return (
-    <div className={state ? "modal--hidden" : "modal"}>
-      <h1>Welcome!</h1>
-      <h2>What should we call you?</h2>
-      <form>
-        <input type="text" onChange={change}></input>
-        <button type="button" onClick={() => onClick()}>
-          Submit
-        </button>
-      </form>
+    <div className={state ? "modal--hidden" : "modal--container"}>
+      <div className="modal">
+        <h1>Welcome!</h1>
+        <h2>What should we call you?</h2>
+        <form>
+          <input type="text" onChange={change}></input>
+          <button type="button" onClick={() => onClick()}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
