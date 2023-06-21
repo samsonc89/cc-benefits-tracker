aee7cc71755d75571cb9277f8ba7789ee2f54550
Samson Cheung <samsonc89@yahoo.com>
2023-06-21 12:31:09 -0700

    Add function to set anniversary date

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,8 +1,35 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
 import "./app.css";
+import { useState } from "react";
 
-const CreditCard = ({ cardData, delBtn, onCheck }) => {
+const CreditCard = ({ cardData, dateChange, onCheck }) => {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const formatter = new Intl.DateTimeFormat("en-us", {
+    month: "2-digit",
+    day: "2-digit",
+  });
+
+  const handleButtonClick = () => {
+    setIsDatePickerOpen(true);
+  };
+
+  const handleDateChange = (event) => {
+    if (!event.target.value == "") {
+      setSelectedDate(new Date(event.target.value + "T00:00:00"));
+    }
+  };
+
+  const handleDatePickerClose = (cardId, date) => {
+    if (!selectedDate == "") {
+      dateChange(cardId, date);
+    }
+
+    setIsDatePickerOpen(false);
+  };
+
   return (
     <>
       <img src={cardData.img} className="content--card--image" />
@@ -11,8 +38,26 @@ const CreditCard = ({ cardData, delBtn, onCheck }) => {
           <tr>
             <th>{cardData.name}</th>
             <th>
-              Anniversary: {cardData.anniversary}
-              <button onClick={() => delBtn(cardData.id)}>Edit</button>
+              Anniversary:{" "}
+              {cardData.anniversary == "" ? "" : cardData.anniversary}
+            </th>
+            <th>
+              <button onClick={handleButtonClick}>Edit</button>
+              {isDatePickerOpen && (
+                <div>
+                  <input type="date" onChange={handleDateChange} />
+                  <button
+                    onClick={() =>
+                      handleDatePickerClose(
+                        cardData.id,
+                        formatter.format(selectedDate)
+                      )
+                    }
+                  >
+                    Set
+                  </button>
+                </div>
+              )}
             </th>
           </tr>
           <tr>
