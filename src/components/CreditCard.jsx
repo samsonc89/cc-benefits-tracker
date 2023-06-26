4b4b9f812986b637e619c9539a50fcf156982e20
Samson Cheung <samsonc89@yahoo.com>
2023-06-26 12:51:56 -0700

    Refactor button to edit date

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -3,6 +3,7 @@
 
 import "./app.css";
 import { useState } from "react";
+import EditButton from "./EditButton.jsx";
 
 const CreditCard = ({ cardData, dateChange, onCheck }) => {
   const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
@@ -42,16 +43,7 @@ const CreditCard = ({ cardData, dateChange, onCheck }) => {
               {cardData.anniversary == "" ? "" : cardData.anniversary}
             </th>
             <th>
-              <button className="btn-edit" onClick={handleButtonClick}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  height="20px"
-                >
-                  <title>pencil-outline</title>
-                  <path d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z" />
-                </svg>
-              </button>
+              <EditButton onClick={handleButtonClick} />
               {isDatePickerOpen && (
                 <div>
                   <input type="date" onChange={handleDateChange} />
